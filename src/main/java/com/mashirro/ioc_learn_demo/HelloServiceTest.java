33e4a67fb16f27f3a3023b3ed988aaac6d34052f
mashirro <1213410472@qq.com>
2020-05-17 15:38:16 +0800

    @Value通常用于注入外部属性

--- src/main/java/com/mashirro/ioc_learn_demo/HelloServiceTest.java
+++ src/main/java/com/mashirro/ioc_learn_demo/HelloServiceTest.java
@@ -3,6 +3,7 @@ package com.mashirro.ioc_learn_demo;
 
 import com.mashirro.ioc_learn_demo.annotation.RountingInjected;
 import com.mashirro.ioc_learn_demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -14,18 +15,30 @@ public class HelloServiceTest {
      * 关于BeanPostProcessor讲解请参看博客Spring5.2.5 ioc容器官方文档学习
      */
     @RountingInjected(beanName = "HelloService2")
-//    @Autowired
-//    @Qualifier("HelloService2")
+    //第二种
+    //@Autowired
+    //@Qualifier("HelloService2")
+    //第三种
+    //@Resource(name = "HelloService2")
     private HelloService helloService;
+
+
+    @Value("${teacher.name}")
+    private String teacherName;
 
     public void sayHello(){
         System.out.println(helloService.sayHello());
     }
 
+    /**
+     * 测试用main入口
+     * @param args
+     */
     public static void main(String[] args) {
         //使用AnnotationConfigApplicationContext实例化Spring容器
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext("com.mashirro.ioc_learn_demo");
         HelloServiceTest helloServiceTest = ac.getBean(HelloServiceTest.class);
         helloServiceTest.sayHello();
+        System.out.println(helloServiceTest.teacherName);
     }
 }
--- src/main/java/com/mashirro/ioc_learn_demo/IocLearnDemoApplication.java
+++ src/main/java/com/mashirro/ioc_learn_demo/IocLearnDemoApplication.java
@@ -2,8 +2,10 @@ package com.mashirro.ioc_learn_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:custom.properties")
 public class IocLearnDemoApplication {
 
 	public static void main(String[] args) {
